ae5d520b8d096b0918a1a925766b864e7bf3aca2
wongArchitect <jodronwong@126.com>
2020-11-13 22:18:16 +0800

    备份文件

--- primary-source/src/main/java/Contents.java
+++ primary-source/src/main/java/Contents.java
@@ -2,7 +2,7 @@ public class Contents {
     static String sourceFilesRootDir = "E:\\GUAN论";
     static String exportFileName = "e:\\guan论-随笔 ";
 
-//    static String startDateStr = "2020-11-04 00:00:00";
-//    static String endDateStr = "2020-11-11 00:00:00";
+//    static String startDateStr = "2020-11-12 00:00:00";
+//    static String endDateStr = "2020-11-13 00:00:00";
 
 }
