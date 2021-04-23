09a419013014768fed5351af797a9e8ce9de4214
wongArchitect <jodronwong@126.com>
2021-04-23 16:45:03 +0800

    备份文件 备份时间：2021-04-23

--- primary-source/src/main/java/Contents.java
+++ primary-source/src/main/java/Contents.java
@@ -3,8 +3,9 @@ public class Contents {
     static String SOURCE_FILES_ROOT_DIR_PRE = "E:\\precipitate\\workspace\\txtExport\\wg _ bak";
     static String EXPORT_FILE_NAME = "E:\\precipitate\\workspace\\txtExport\\wg _ bak\\doc文档\\WG论-随笔 ";
 
-    static String START_DATE_STR = "2020-11-11 00:00:00";
-    static String END_DATE_STR = "2021-04-05 00:00:00";
+    static String START_DATE_STR = "2021-04-05 00:00:00";
+    static String END_DATE_STR = "2021-04-22 00:00:00";
 
     static String TIME_QUANTUM = "DoNotTimeQuantum";
+//    static String TIME_QUANTUM = "DoTimeQuantum";
 }
