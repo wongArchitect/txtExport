dbd5ce2cf282696a0fd00b112bd215cf836301ac
wongArchitect <jodronwong@126.com>
2021-05-03 23:14:35 +0800

    修改导出文档的目录，并整理备份文件

--- primary-source/src/main/java/Contents.java
+++ primary-source/src/main/java/Contents.java
@@ -1,11 +1,11 @@
 public class Contents {
     static String SOURCE_FILES_ROOT_DIR = "E:\\precipitate\\workspace\\txtExport\\wg _ bak\\WG论";
     static String SOURCE_FILES_ROOT_DIR_PRE = "E:\\precipitate\\workspace\\txtExport\\wg _ bak";
-    static String EXPORT_FILE_NAME = "E:\\precipitate\\workspace\\txtExport\\wg _ bak\\doc文档\\WG论-随笔 ";
+    static String EXPORT_FILE_NAME = "E:\\precipitate\\workspace\\txtExport\\wg _ bak\\WG_bak\\WG_doc_bak\\WG论-随笔 ";
 
     static String START_DATE_STR = "2021-04-05 00:00:00";
-    static String END_DATE_STR = "2021-04-22 00:00:00";
+    static String END_DATE_STR = "2021-05-03 00:00:00";
 
-    static String TIME_QUANTUM = "DoNotTimeQuantum";
-//    static String TIME_QUANTUM = "DoTimeQuantum";
+//    static String TIME_QUANTUM = "DoNotTimeQuantum";
+    static String TIME_QUANTUM = "DoTimeQuantum";
 }
